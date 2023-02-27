ec6c7a5ebfb9b7fe2edefd039e7b69e2395a4928
Reinier <rnuviola@gmail.com>
2023-02-27 15:08:15 -0500

    Delete unnecessary files

--- src/components/SideBar/HeaderBar.js
+++ src/components/SideBar/HeaderBar.js
@@ -3,14 +3,14 @@ import styles from "./HeaderBar.module.css";
 
 const HeaderBar = (props) => {
   return (
-    <div className="text-center d-flex align-items-center justify-content-center mt-2">
+    <div className="text-center d-flex align-items-center justify-content-center mt-3 mb-2">
       {props.isCollapse ? (
         <h1 className={`${styles['logo']} display-6 text-white`}>
           <i class="bx bxs-doughnut-chart"></i>
         </h1>
       ) : (
         <div
-          className={`${styles["logo-name"]} d-flex align-items-center justify-content-center`}
+          className={`${styles["logo-name"]} d-flex align-items-center me-4 pe-2`}
         >
           <i className="bx bxs-doughnut-chart bx-md me-1"></i> DataCog
         </div>
